aca89044af10647efa80dc54b84f178060932ae5
plzombie <plzombie@yandex.ru>
2025-01-08 04:08:15 +0300

    Probable fix for 51

--- src/stages/fix_orientation/OptionsWidget.h
+++ src/stages/fix_orientation/OptionsWidget.h
@@ -41,6 +41,8 @@ public:
 
     virtual ~OptionsWidget();
 
+    bool disconnectAll(void);
+
     void preUpdateUI(PageId const& page_id, OrthogonalRotation rotation);
 
     void postUpdateUI(OrthogonalRotation rotation);
